ad53c715623f4f22ba32b63fbd0a4f9ed36a6c6d
bigkenbird <k918231524@gmail.com>
2023-02-12 00:49:25 +0800

    build spring start project

---
+++ src/main/java/com/cub/demo/CubdemoApplication.java
@@ -0,0 +1,13 @@
+package com.cub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubdemoApplication.class, args);
+	}
+
+}
